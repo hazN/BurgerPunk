0836782f2b45d9449551dfef89e6721f7847ce74
Krupesh Parmar <parmarkrupesh786@gmail.com>
2025-04-21 15:20:34 -0400

    customer can now eat and leave

---
+++ Assets/_Scripts/1_NPC/CustomerStates/AnimStates/EatAndLeave.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class EatAndLeave : StateMachineBehaviour
+{
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        CustomerBehaviour customer = animator.gameObject.GetComponent<CustomerBehaviour>();
+        if (customer != null)
+        {
+            if(customer.IsOrderFulfilled)
+            {
+                customer.TransitionToState(customer.mCustomerMovingState);
+            }
+            else
+            {
+                customer.IsOrderFulfilled = true;
+                customer.Animator.SetBool(customer.m_HashSit, false);
+                customer.Stool.gameObject.SetActive(false);
+            }
+        }
+    }
+}
