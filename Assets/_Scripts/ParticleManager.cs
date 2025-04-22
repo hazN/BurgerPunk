d3ba5a82913a1226b2b3b872e12f3b50583317d0
Richard <elitegem84@gmail.com>
2025-04-22 01:25:38 -0400

    build particle and particle manager

---
+++ Assets/_Scripts/ParticleManager.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ParticleManager : MonoBehaviour
+{
+    public static ParticleManager Instance;
+    [SerializeField] private Particle[] types;
+    [SerializeField] private GameObject[] effects;
+    private Dictionary<Particle, GameObject> particles;
+    public enum Particle
+    {
+        Pop
+    }
+
+    private void Start()
+    {
+        int size = types.Length;
+        particles = new Dictionary<Particle, GameObject>();
+        for (int i = 0; i < size; i++)
+        {
+            particles.Add(types[i], effects[i]);
+        }
+    }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void CreateParticleEffect(Particle type, Vector3 pos, float destroyTime)
+    {
+        GameObject effect = Instantiate(particles[type], pos, Quaternion.identity);
+        effect.GetComponent<ParticleSystem>().Play();
+        Destroy(effect, destroyTime);
+    }
+
+}
